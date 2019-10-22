cddc6073bdfcd570d63d7f505c011ad674750e92
杨浩政 <53072554+sheeep996@users.noreply.github.com>
2019-10-22 09:08:06 +0800

    Add files via upload

--- float-equal.c
+++ float-equal.c
@@ -6,6 +6,12 @@ int main()
     float m = 1.78;
     float x = 1.779999, y = 2;
     float z = m / y, z1 = x / y;
+
+    float d = 0.00001;
+  	if (((z-z1 < d)&&(z - z1 > 0))||((z1 - z < d)&&(z1 - z >0))){
+  		z = z1;
+	  }
+    
     if (z == z1) {
         z = (m + x) / y;
     }
